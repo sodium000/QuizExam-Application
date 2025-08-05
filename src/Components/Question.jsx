f823574602bbde98ff937e94dbc518a7deb2c896
Raisul Islam Tonmoy <sodiumrdx@gmail.com>
2025-08-06 01:22:55 +0600

    name change

--- src/Components/Question.jsx
+++ src/Components/Question.jsx
@@ -6,7 +6,7 @@ export default function Question() {
     <div className={classes.question}>
       <div className={classes.qtitle}>
         <span className="material-icons-outlined"> help_outline </span>
-        Here goes the question from Learn with Sumit?
+        Here goes the question from this topic? 
       </div>
       <Answers />
     </div>
